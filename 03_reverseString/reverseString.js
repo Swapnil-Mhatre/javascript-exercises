387a397afc668d30653962c6420de57a31e35639
Swapnil-Mhatre <swapmhatre889@gmail.com>
2023-10-26 16:17:10 +0530

    Third JavaScript Exercise Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringValue) {
+    let reverseString = "";
+    for (let i = stringValue.length - 1; i >= 0; i--) {
+        reverseString += stringValue.charAt([i]);
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
