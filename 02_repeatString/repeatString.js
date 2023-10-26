ec1320e58fcfca892d6fce96ebc47f3ec9dbed7f
Swapnil-Mhatre <swapmhatre889@gmail.com>
2023-10-26 15:51:12 +0530

    Second Exercise Completed of JavaScript

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let countString = "";
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    for (let i = 0; i < num; i++) {
+        countString += string;
+    }
+    return countString;
 };
 
 // Do not edit below this line
