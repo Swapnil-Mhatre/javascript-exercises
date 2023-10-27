3086952f102be4a2f96dc25d20c596495796a401
Swapnil-Mhatre <swapmhatre889@gmail.com>
2023-10-27 13:37:22 +0530

    JavaScript 5th Exercise Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    let sumNum = 0 ;
+    if (startNum < 0 || endNum < 0) return "ERROR";
+    if (startNum > endNum) {
+        for (let i = endNum; i <= startNum; i++) {
+            sumNum += i;
+        }
+    } else{
+        for (let i = startNum; i <= endNum; i++) {
+            sumNum += i;
+        }
+    } 
+    if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) return "ERROR";
+    return sumNum;
 };
 
 // Do not edit below this line
